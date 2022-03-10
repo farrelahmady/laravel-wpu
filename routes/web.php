3a5f368c357c13b4b43d44b90829b8dc5668dc54
Farrel Ahmad Yudithia <farrelfay.ce@gmail.com>
2022-03-10 09:03:00 +0700

    folder structure, route, view (add home, about, blog)

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,17 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+   return view('home');
+});
+
+Route::get('/about', function () {
+   return view('about', [
+      "name" => "Farrel Ahmad Yudithia",
+      "email" => "farrelfay.ce@gmail.com",
+      "image" => "farrel.webp"
+   ]);
+});
+
+Route::get('/blog', function () {
+   return view('blog');
 });
